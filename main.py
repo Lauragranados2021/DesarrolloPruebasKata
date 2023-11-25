6968305cb57a8b3f3ef24182fb856543c9d6d08f
Lauragranados2021 <laura.granados05@uptc.edu.co>
2023-11-25 12:58:52 -0500

    Codigo Protegido

--- main.py
+++ main.py
@@ -2,88 +2,94 @@
 def challenge_one():
     vectorEntrada=[]
     vectorSalida = []
-    #pedimos el tamaño del vector y verificamos que sea menor a 100
-    n = int(input("Ingrese la cantidad de digitos dentro de su vector (menor a 100): "))
-    if(n<=100):
-        #un ciclo para pedir los digitos
-        while len(vectorEntrada)<n:
+    try:
+        #pedimos el tamaño del vector y verificamos que sea menor a 100
+        n = int(input("Ingrese la cantidad de digitos dentro de su vector (menor a 100): "))
+        if(n<=100):
+            #un ciclo para pedir los digitos
+            while len(vectorEntrada)<n:
 
-            num = input("Ingrese los digitos:  ")
-            vectorEntrada.append(num)
+                num = input("Ingrese los digitos:  ")
+                vectorEntrada.append(num)
 
-            #empieza en la ultima posicion de mi vector y va de para atras, va restando una posicion desde las mas grande  , acaba hasta que ya no haya posicones osea cuando encuentra la posicione -1 falla
-        for i in range(len(vectorEntrada) - 1, -1, -1):
-            #Lo vuelvo a String para poder separarlo
-            separar=str(vectorEntrada[i])
+                #empieza en la ultima posicion de mi vector y va de para atras, va restando una posicion desde las mas grande  , acaba hasta que ya no haya posicones osea cuando encuentra la posicione -1 falla
+            for i in range(len(vectorEntrada) - 1, -1, -1):
+                #Lo vuelvo a String para poder separarlo
+                separar=str(vectorEntrada[i])
 
-            #esta linea recorre los elementos dentro de separar que ya son string  los vuelve a enteros y los guarda en div
-            div = [int(digito) for digito in separar]
-            for e in div:
-                #Compruebo mi rango
-                if e<6:
-                    vectorSalida.append(e)
-        print (vectorEntrada)
-        print (vectorSalida)
-    else:
-        print("Usted escribio un numero mayor a 100, la entrada no es valida ")
-
+                #esta linea recorre los elementos dentro de separar que ya son string  los vuelve a enteros y los guarda en div
+                div = [int(digito) for digito in separar]
+                for e in div:
+                    #Compruebo mi rango
+                    if e<6:
+                        vectorSalida.append(e)
+            print (vectorEntrada)
+            print (vectorSalida)
+        else:
+            print("Usted escribio un numero mayor a 100, la entrada no es valida ")
+    except Exception as e:
+        print('digitaste un valor incorrecto ')
 
 def challenge_two():
     vectorEntrada = []
     vectorSalida = []
-    # pedimos el tamaño del vector y verificamos que no sea vacio
-    n = int(input("Ingrese la cantidad de digitos dentro de su vector debe ser mayor a 0 "))
-    if (n>0):
-        # un ciclo para pedir los digitos
-        while len(vectorEntrada) < n:
-            num = input("Ingrese los digitos:  ")
-            vectorEntrada.append(num)
+    try:
+        # pedimos el tamaño del vector y verificamos que no sea vacio
+        n = int(input("Ingrese la cantidad de digitos dentro de su vector debe ser mayor a 0 "))
+        if (n>0):
+            # un ciclo para pedir los digitos
+            while len(vectorEntrada) < n:
+                num = input("Ingrese los digitos:  ")
+                vectorEntrada.append(num)
 
-        print( vectorEntrada)
+            print( vectorEntrada)
 
-        for e in vectorEntrada:
-                cuadrado=pow(int(e),2)
-                if cuadrado<=36:
-                    vectorSalida.append(cuadrado)
-        print(vectorSalida)
-        # el primer for comprueba que los numeros esten en la posicion correcta
-        for i in range(len(vectorSalida)-1):
-            #el segundo comprueba entre dos numeros, primera con segunda, segunda con tercera ... para poner al mayor en la parte izquierda
-            for j in range(0,len(vectorSalida)-i-1):
-                if vectorSalida[j]>vectorSalida[j+1]:
-                    #cambia los valores de ambas posicones
-                    vectorSalida[j],vectorSalida[j+1]=vectorSalida[j+1],vectorSalida[j]
+            for e in vectorEntrada:
+                    cuadrado=pow(int(e),2)
+                    if cuadrado<=36:
+                        vectorSalida.append(cuadrado)
+            print(vectorSalida)
+            # el primer for comprueba que los numeros esten en la posicion correcta
+            for i in range(len(vectorSalida)-1):
+                #el segundo comprueba entre dos numeros, primera con segunda, segunda con tercera ... para poner al mayor en la parte izquierda
+                for j in range(0,len(vectorSalida)-i-1):
+                    if vectorSalida[j]>vectorSalida[j+1]:
+                        #cambia los valores de ambas posicones
+                        vectorSalida[j],vectorSalida[j+1]=vectorSalida[j+1],vectorSalida[j]
 
-        print(vectorSalida)
-    else:
-        print("Su vector no debe estar vacio")
-
+            print(vectorSalida)
+        else:
+            print("Su vector no debe estar vacio")
+    except Exception as e:
+        print('digitaste un valor incorrecto ')
 def challenge_three():
     vectorEntrada = []
-    n = int(input("Ingrese el tamaño de su matriz: "))
-    if (n>0):
-        # un ciclo para pedir los digitos
-        while len(vectorEntrada) < n:
-            num = int(input("Ingrese el valor de sus monedas. Recuerde que deben ser positivos:  "))
-            if num>0:
-                vectorEntrada.append(num)
-            else:
-                print("usted agrego una moneda negativa, eso no existe")
-                #Primero lo ordeno
-        vectorEntrada.sort()
-        #el minimo cambio que puedo hacer es 1 pero ira sumando a medida que encuentre mas monedas de otros valores
-        cambio=1
-        for i in vectorEntrada:
-            #compruebo que el cambio sea menor a la moneda que tengo, si llega a ser mayor entonces significa que ese es mi mayor
-            if(i<=cambio):
-                cambio=cambio+i
-            else:
-                break
+    try:
+        n = int(input("Ingrese el tamaño de su matriz: "))
+        if (n>0):
+            # un ciclo para pedir los digitos
+            while len(vectorEntrada) < n:
+                num = int(input("Ingrese el valor de sus monedas. Recuerde que deben ser positivos:  "))
+                if num>0:
+                    vectorEntrada.append(num)
+                else:
+                    print("usted agrego una moneda negativa, eso no existe")
+                    #Primero lo ordeno
+            vectorEntrada.sort()
+            #el minimo cambio que puedo hacer es 1 pero ira sumando a medida que encuentre mas monedas de otros valores
+            cambio=1
+            for i in vectorEntrada:
+                #compruebo que el cambio sea menor a la moneda que tengo, si llega a ser mayor entonces significa que ese es mi mayor
+                if(i<=cambio):
+                    cambio=cambio+i
+                else:
+                    break
 
-        print( vectorEntrada)
-        print(cambio)
+            print( vectorEntrada)
+            print(cambio)
 
-
+    except Exception as e:
+        print('digitaste un valor incorrecto ')
 while True:
     print("\nMenú:")
     print("1. Desafío 1")
@@ -104,4 +110,3 @@ while True:
         break
     else:
         print("Opción no válida. Por favor, elija un número del 1 al 4.")
-
